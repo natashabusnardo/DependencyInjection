37e14f222852cd010c5a48ce199b3d7b07e355b8
Natasha <natashabusnardo@gmail.com>
2021-05-12 17:07:05 -0300

    Model Classes

---
+++ src/Aluno.java
@@ -0,0 +1,64 @@
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+import java.util.Date;
+
+public class Aluno extends Pessoa {
+	private int matricula;
+	private int cpf;
+	private Date dataNasc;
+	private String email;
+
+	public int getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+
+	public int getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+
+	public Date getDataNasc() {
+		return dataNasc;
+	}
+
+	public void setDataNasc(String dataNasc) {
+		try {
+			this.dataNasc = new SimpleDateFormat("dd/MM/yyyy").parse(dataNasc);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		if (email.length() > 0)
+			this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Aluno [matricula=");
+		builder.append(matricula);
+		builder.append(", CPF=");
+		builder.append(cpf);
+		builder.append(", dataNasc=");
+		builder.append(dataNasc);
+		builder.append(", email=");
+		builder.append(email);
+		builder.append("]");
+		return builder.toString();
+	}
+
+}
